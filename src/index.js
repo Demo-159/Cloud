691b0d92fbf27d5ac327412de34227435caa9a6e
Demo-159 <davidmonrroy7@gmail.com>
2025-06-18 21:16:42 -0400

    Actualizar index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 export default {
   async fetch(request) {
-    const videoUrl = "https://fin-3dg-b1.i8yz83pn.com/hls2/01/09265/7bhx68umfv5q_x/master.m3u8?t=ZWFq20T8foCD_HgBIQaZ968ImWq3EqXk83joCof4GPM&s=1750295360&e=10800&f=46327812&srv=26&asn=27901&sp=5500&p=";
+    const videoUrl = "https://fin-3dg-b1.i8yz83pn.com/hls2/01/09265/7bhx68umfv5q_x/index-v1-a1.m3u8?t=ZWFq20T8foCD_HgBIQaZ968ImWq3EqXk83joCof4GPM&s=1750295360&e=10800&f=46327812&srv=26&asn=27901&sp=5500&p=";
 
     const response = await fetch(videoUrl, {
       headers: {
